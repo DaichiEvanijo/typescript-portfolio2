5dc4547544ec2a850cd993494ce0a8b9d282ea63
iamdaichi <takanogi2468@gmail.com>
2024-01-13 12:02:48 +0100

    useWindowsize revised

--- src/app/useWindowSize.tsx
+++ src/app/useWindowSize.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export const useWindowSize = () =>{
-  const [windowSize, setWindowSize] = useState(1920)
+  const [windowSize, setWindowSize] = useState(200)
   
   useEffect(() =>{
     const handleWindowSizChange = () =>{
